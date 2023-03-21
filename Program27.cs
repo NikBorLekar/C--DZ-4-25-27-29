de2e0b04970f2fd62b2824738bc65cd3eef20feb
Nikita_Borisov <126783560+NikBorLekar@users.noreply.github.com>
2023-03-21 12:30:08 +0700

    Add files via upload

---
+++ Program27.cs
@@ -0,0 +1,25 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.WriteLine("Введите число N: ");
+int numberN = Convert.ToInt32(Console.ReadLine());
+
+  int Sum(int numberN)
+  {
+    int count = Convert.ToString(numberN).Length;
+    int advance = 0;
+    int result = 0;
+
+    for (int i = 0; i < count; i++)
+    {
+      advance = numberN - numberN % 10;
+      result = result + (numberN - advance);
+      numberN = numberN / 10;
+    }
+   return result;
+  }
+
+int sum = Sum(numberN);
+Console.WriteLine("Сумма чисел равна: " + sum);
---
+++ Program29.cs
@@ -0,0 +1,84 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+string? Numbers = Console.ReadLine();
+
+Numbers = Numbers + ",";    
+
+//удаление пробелов из строки 
+string DelSpace (string series)
+{
+  string New = "";
+  for (int i = 0; i < series.Length; i++)
+  {
+    if (series[i] != ' ') 
+    {
+      New += series[i];
+    }
+  }
+  return New;
+}
+
+// проверка на правильность ввода 
+void СheckNumber2 (int  series)
+{
+
+      if (series == '0'||series == '1'||series == '2'
+      ||series == '3'||series == '4'||series == '5'||series == '6'
+      ||series == '7'||series == '8'||series == '9'||series == ','
+      ||series == '-')
+      {
+      }
+        else 
+        {
+          Console.WriteLine($"Ошибка ввода, введите цифры");
+
+      }
+}
+
+// создание и заполнение массива из строки
+int[] ArrayOfNumbers(string seriesNew)
+{ 
+
+  int[] arrayOfNumbers = new int[1];    // создаем массив из 1 элемента
+
+  int j =0;
+  for (int i = 0; i < seriesNew.Length; i++)
+  {
+    string seriesNew1 = "";
+
+    while (seriesNew[i] != ',' && i < seriesNew.Length){
+      seriesNew1 += seriesNew[i];
+      СheckNumber2(seriesNew[i]);
+      i++;
+    }
+    arrayOfNumbers[j] = Convert.ToInt32(seriesNew1);    // заполняет массив значениями из строки
+    if (i < seriesNew.Length-1)
+    {
+      arrayOfNumbers = arrayOfNumbers.Concat(new int[] {0}).ToArray();    // добавляет новый нулевой элемент в конец массива
+    }
+    j++;
+  }
+  return arrayOfNumbers;
+}
+
+// вывод массива на печать 
+void PrintArray(int[] coll)
+{
+  int count = coll.Length;
+  int index = 0;
+  Console.Write("[");
+  while(index < count)
+  {
+    Console.Write(coll[index]);
+    index++;
+    if (index < count)
+    {
+      Console.Write(", ");
+    }
+  }
+  Console.Write("]");
+} 
+
+string seriesNew = DelSpace(Numbers);
+int[] arrayOfNumbers =  ArrayOfNumbers(seriesNew);
+PrintArray(arrayOfNumbers);
